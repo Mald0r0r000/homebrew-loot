ba5abe133189cec92ee37584b8e761afb3c34171
Mald0r0r000 <antoinebedos@gmail.com>
2026-03-23 23:52:20 +0100

    bump lootup to v0.1.2 — interactive TUI

--- lootup.rb
+++ lootup.rb
@@ -5,8 +5,8 @@ class Lootup < Formula
 
   # Stable Release via Git Tag
   url "https://github.com/Mald0r0r000/LOOTup.git",
-      tag:      "v0.1.1",
-      revision: "64ae888f4096f62c46c2e77575a060aebbe19a9a"
+      tag:      "v0.1.2",
+      revision: "6c2c48d6720558073b9be1f2e7b1e0720454164f"
 
   head "https://github.com/Mald0r0r000/LOOTup.git", branch: "main"
 
