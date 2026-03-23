d2cae13fc7e9b8ed787d6dd542a0020d7c969f93
Mald0r0r000 <antoinebedos@gmail.com>
2026-03-23 23:35:29 +0100

    bump lootup to v0.1.1

--- lootup.rb
+++ lootup.rb
@@ -5,8 +5,8 @@ class Lootup < Formula
 
   # Stable Release via Git Tag
   url "https://github.com/Mald0r0r000/LOOTup.git",
-      tag:      "v0.1.0",
-      revision: "7cff752cc8b26308b87c80dc51b9a614cc26d236"
+      tag:      "v0.1.1",
+      revision: "64ae888f4096f62c46c2e77575a060aebbe19a9a"
 
   head "https://github.com/Mald0r0r000/LOOTup.git", branch: "main"
 
